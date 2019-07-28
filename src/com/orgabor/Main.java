2cceeb718dc1b7ad4f9db6c267e81a466d77d895
Gabor <orbangabeszb@gmail.com>
2019-07-28 11:40:54 +0200

    instructions added with Scanner(System.in)

---
+++ src/com/orgabor/Main.java
@@ -0,0 +1,31 @@
+package com.orgabor;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("Üdv a RenameForAnyában! :)");
+
+		//asking for a source dir
+		System.out.println("Add meg a forrásmappa helyét: ");
+		String sourceDir = sc.nextLine();
+
+		//asking for destination dir
+		System.out.println("Add meg a célmappát: ");
+		String destDir = sc.nextLine();
+		
+		//asking for a common name for the renamed files
+		System.out.println("Mi legyen a fájlok közös neve? (pl. dátum-helyszín");
+		String commonName = sc.nextLine();
+		
+		//closing scanner, the program runs on it's own from here
+		sc.close();
+		System.out.println("Köszi, a fájlok feldolgozása megkezdõdött!");
+		
+		
+	}
+
+}
