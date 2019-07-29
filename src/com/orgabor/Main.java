0d6fa610dd638504610c018a78d2dc8e5e25dc14
Gabor <orbangabeszb@gmail.com>
2019-07-29 13:10:00 +0200

    program creates non-existing destination folder

--- src/com/orgabor/Main.java
+++ src/com/orgabor/Main.java
@@ -1,5 +1,8 @@
 package com.orgabor;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,22 +12,35 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Üdv a RenameForAnyában! :)");
 
-		//asking for a source dir
-		System.out.println("Add meg a forrásmappa helyét: ");
-		String sourceDir = sc.nextLine();
-
-		//asking for destination dir
-		System.out.println("Add meg a célmappát: ");
-		String destDir = sc.nextLine();
-		
-		//asking for a common name for the renamed files
-		System.out.println("Mi legyen a fájlok közös neve? (pl. dátum-helyszín");
-		String commonName = sc.nextLine();
+//		//asking for a source dir
+//		System.out.println("Add meg a forrásmappa helyét: ");
+//		String sourceDir = sc.nextLine();
+//
+//		//asking for destination dir
+//		System.out.println("Add meg a célmappát: ");
+//		String destDir = sc.nextLine();
+//		
+//		//asking for a common name for the renamed files
+//		System.out.println("Mi legyen a fájlok közös neve? (pl. dátum-helyszín)");
+//		String commonName = sc.nextLine();
 		
 		//closing scanner, the program runs on it's own from here
 		sc.close();
 		System.out.println("Köszi, a fájlok feldolgozása megkezdõdött!");
 		
+		//initializing source and destination folders
+		File sourceFolder = new File("C:\\Users\\Gábor\\Desktop\\testRes");
+		File destFolder = new File("C:\\Users\\Gábor\\Desktop\\testDest");
+		if(!destFolder.exists()) {
+			destFolder.mkdirs();
+		}
+		
+//		for(File fileEntry : sourceFolder.listFiles()) {
+//			System.out.println(fileEntry.getName());
+//		}
+	}
+	
+	public static void copyImgs(File source, File destination, String newFileName) {
 		
 	}
 
