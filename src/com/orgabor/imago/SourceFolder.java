32246edeaa74ee106454a5b76c669c498f8d0a08
Gabor <orbangabeszb@gmail.com>
2019-08-25 21:01:07 +0200

    BufferedImage class discarded for Files.copy()

--- src/com/orgabor/imago/SourceFolder.java
+++ src/com/orgabor/imago/SourceFolder.java
@@ -1,9 +1,9 @@
 package com.orgabor.imago;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 class SourceFolder extends File {
     //numbering of images
@@ -18,27 +18,21 @@ class SourceFolder extends File {
         for (File fileEntry : this.listFiles()) {
             //checks if entry is image file and skips if not
             if (fileEntry.isFile()) {
-                if (readImg(fileEntry) == null) {
+                if (ImageIO.read(fileEntry) == null) {
                     continue;
                 }
             } else {
                 continue;
             }
-            BufferedImage bImg = readImg(fileEntry);
+
             File copy = new File(destFolderPathname,
                     nameFile(fileEntry, newFileName, serialNumber));
-            ImageIO.write(bImg, getExtension(fileEntry), copy);
+
+            Files.copy(fileEntry.toPath(), copy.toPath());
+
             serialNumber++;
         }
         serialNumber -= 1;
-    }
-
-    //reads image file and returns BufferedImage, called in copyImgs,
-    // exception handled in processFields @Controller
-    private BufferedImage readImg(File file) throws IOException {
-        BufferedImage img = null;
-        img = ImageIO.read(file);
-        return img;
     }
 
     //names the new file with numbering and extension
